db824e783421793781fe61b802353fb9d5524372
edouardparis <m@edouard.paris>
2025-01-16 12:24:38 +0100

    fix release script: use

--- contrib/release/sign.sh
+++ contrib/release/sign.sh
@@ -4,7 +4,7 @@ set -e  # Exit immediately if a command exits with a non-zero status
 set -x  # Print commands and their arguments as they are executed
 
 VERSION="${VERSION:-"9.0"}"
-LIANA_PREFIX="liana-$VERSION"
+LIANA_PREFIX="liana-$VERSION-21st-capital"
 # Define the release directory
 RELEASE_DIR="$PWD/release_assets"
 RELEASE_BUILD_DIR="$PWD/release_build"
@@ -85,9 +85,9 @@ sign_with_rcodesign() {
         --api-key-path "$NOTARY_API_CREDS_FILE" \
         --staple Liana.app
 
-    zip_archive "Liana-$VERSION.zip" Liana.app
-    mv "Liana-$VERSION.zip" "$RELEASE_DIR/"
-    sha256sum "$RELEASE_DIR/Liana-$VERSION.zip" | tee -a "$RELEASE_DIR/$LIANA_PREFIX-shasums.txt"
+    zip_archive "$LIANA_PREFIX-macos.zip" Liana.app
+    mv "$LIANA_PREFIX-macos.zip" "$RELEASE_DIR/"
+    sha256sum "$RELEASE_DIR/$LIANA_PREFIX-macos.zip" | tee -a "$RELEASE_DIR/$LIANA_PREFIX-shasums.txt"
 }
 
 if [ "$#" -lt 1 ]; then
